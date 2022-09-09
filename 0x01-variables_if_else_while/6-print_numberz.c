80772f98749b720f6a9f381f652c55076dd1a120
heran <hello@ufirstdev>
2022-09-09 11:47:36 -0700

    print number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,16 @@
 /**
 * main - print if the number is positive, zero, or negative
 *
-* Description: using main function
-*this program prints "Programming is positive, zero, or negative
-* Return: 0
+* Description: using the main function
+*this program prints "programming is positive, zero ,or negative
+*Return: 0
 */
 int main(void)
 {
 int n;
-for (n = '0' ; n <= '9' ; n++ )
+for (n = '0'; n <= '9'; n++)
 {
-         putchar(n) ;
-}
-putchar( ' \n' ) ;
-return (0) ;
+      putchar(n);
+}putchar('\n');
+return (0);
 }
