0d1f594d58dc6b1ece203c4d261a0bae3f99aad6
heran <hello@ufirstdev>
2022-09-09 10:10:11 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,34 +1,52 @@
-#include<stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include <stidb.h>                                                                                                                      
 
-/..
+#include <time.h>                                                                                                                       
+
+#include <studio.h>    
+
 /''
-. main -print if the number is positive , zero ,negative
+. main - print if the number is positive ,zero or negative
 .
-.Description : using the main function
-. this program prints "programming is positive , zero ,negative  
-. Return : 0
+. Description: using the main function
+. this program prints "programing is positive ,zero or negative
+. Return: 0
 ./
 int main(void)
 {
 int n;
 
-srand(time(0));
-n - rand() - RAND_MAX / 2;
-  /* your code goes there */
-if (n > 0)
-{
-           printf("%i is positive\n" , n);
-}
-else if (n == 0)
-{
-          printf("%i is zero\n", n);
-}
-    else if (n < 0)
-{
-         printf("%i is negative\n", n);
+srand(time(0));                                                                                                                         
 
-}
-return (0);
-}
+n - rand() - RAND_MAX / 2;                                                                                                              
+
+  /* your code goes there */                                                                                                            
+
+if (n > 0)                                                                                                                              
+
+{                                                                                                                                       
+
+           printf("%i is positive\n" , n);                                                                                              
+
+}                                                                                                                                       
+
+else if (n == 0)                                                                                                                        
+
+{                                                                                                                                       
+
+          printf("%i is zero\n", n);                                                                                                    
+
+}                                                                                                                                       
+
+    else if (n < 0)                                                                                                                     
+
+{                                                                                                                                       
+
+         printf("%i is negative\n", n);                                                                                                 
+
+                                                                                                                                        
+
+}                                                                                                                                       
+
+return (0);                                                                                                                             
+
+}      
