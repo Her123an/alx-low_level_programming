8709687ff2bb19e48796c999b2fbded272c4b405
heran <hello@ufirstdev>
2022-09-10 00:04:20 -0700

    When I was having that alphabet soup, I never thought that it would pay off
    mandatory

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+
+#include <stdio.h>
+
+#include <time.h>
+
+/**
+
+ * main - Entry point
+
+ *
+
+ * Return: Always success
+
+ */
+
+int main(void)
+
+{
+
+    char Low, e,q;
+
+    e = 'e';
+
+    q = 'q';
+
+for (Low = 'a'; low <= 'z'; Low++)
+
+{
+
+     if (Low != e && Low != q)
+
+              putchar(Low);
+
+}
+
+printf("\n");
+
+return (0);
+
+}
