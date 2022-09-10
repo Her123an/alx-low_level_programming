414af5999673a6084146f13121fba276b1b69743
heran <hello@ufirstdev>
2022-09-09 23:44:54 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -22,11 +22,11 @@
 
  *
 
-*Return: always 0.
+ * Return: always 0.
 
 */
 
-int main(void)
+ int main(void)
 
 {
 
@@ -56,9 +56,9 @@ int main(void)
 
             return (0);
 
-   
-
 }
+
+   
 
              
 
