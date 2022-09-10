ffed0cba091aceab5822fefddea872f5e38e6468
heran <hello@ufirstdev>
2022-09-09 22:51:41 -0700

    positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,68 @@
+/*
+
+* file: 0-positive_or_negative.c
+
+*
+
+*/
+
+
+
 #include <stdlib.h>
+
 #include <time.h>
+
 #include <stdio.h>
 
-/**
-* main - print if the number is positive, zero, or negative
-*
-* Description: using the main function
-* this program prints "programming is positive, zero, or negative   
-* Return: 0
-*/
-int main(void)
-{
-int n;
 
-srand(time(0);
-n = rand() - rand_max / 2;
-   /* your code goes there */
-if (n>0)
+
+/**
+
+ * main - prints a random number and states whether it is positive, negative, or zero.
+
+ *
+
+*Return: always 0.
+
+*/
+
+int main(void)
+
 {
-       printf("%i is positive\n", n);
+
+     int n;
+
+
+
+      srand(time(0));
+
+      n = rand() - RAND_MAX / 2;
+
+
+
+       if (n > 0)
+
+              printf("%d is positive\n", n);
+
+       else if (n < 0)
+
+              printf("%d is negative\n", n);
+
+       else
+
+               printf("%d is zero\n", n);
+
+           
+
+            return (0);
+
+   
+
 }
-else if (n == 0)
-{
-         printf("%i is zero\n", n);
-}
-else if (n < 0)
-{
-         printf("%i is negative\n", n);
-}
-return (0);
+
+             
+
+
+
+
+
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,56 +1,8 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                                                                                                                     
-
-* main - prints random number to variable                                                                                               
-
-*                                                                                                                                       
-
-* Return: always (successful)                                                                                                           
-
-*/                                                                                                                                      
-
-int main(void)                                                                                                                          
-
-{                                                                                                                                       
-
-  int n;                                                                                                                                
-
-  char last[] = "Last digit of";                                                                                                        
-
-                                                                                                                                        
-
-   srand(time(0));                                                                                                                      
-
-    n = rand() - RAND_MAX / 2;                                                                                                          
-
-     printf("%s %d is %d and is" , last ,n ,n % 10);                                                                                    
-
-     if (n % 10 > 5)                                                                                                                    
-
-{                                                                                                                                       
-
-           printf("greater than 5\n");                                                                                                  
-
-}                                                                                                                                       
-
-else if (n % 10 == 0)                                                                                                                   
-
-{                                                                                                                                       
-
-   printf("0\n");                                                                                                                       
-
-}                                                                                                                                       
-
-else                                                                                                                                    
-
-{                                                                                                                                       
-
-    printf("less than 6 and not 0\n");                                                                                                  
-
-}                                                                                                                                       
-
-return (0);                                                                                                                             
-
-}        
+/**
+* main - print if the number is positive, zaero, or negative
+*
+* Description: using the main function
+* this program prints "
